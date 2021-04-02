bbe42b6a61ad39ed9952c2305c7b0bc6df2ed26b
doem97 <doem1997@gmail.com>
2021-04-02 17:27:20 +0800

    fix and use 1 mat rather than multiple mat jpg

--- Calculate_F1_Score.py
+++ Calculate_F1_Score.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[48]:
 
 
-from IPython.display import display
-from PIL import Image
 import numpy as np
 from scipy.io import loadmat
 import caffe
 from sklearn.metrics import f1_score, recall_score
 import cv2
 
-
+#======== Define util functions ====================================
 def create_circular_mask(h, w, center=None, radius=None):
     # Create a circular mask from center on an (h,w) map with euclidean distance radius
     if center is None: # use the middle of the image
@@ -37,8 +35,10 @@ def vpg_lane_f1(map1, map2, mask_R = 12):
 
     # First extend the grid-labeled map2 to circle-labeled map extend_mask with boundary R
     
-    map1_mask = map1 > 0
     map2_mask = map2 > 0 # Assume map1,2 only have one class
+    if True not in map2: # If all black map in groundtruth, set as 1 rather than 0 (default zero)
+        return 1.0
+    map1_mask = map1 > 0
     extend_mask = np.zeros((480, 640), dtype=bool) # extended groundtruth (from 8*8 square grid to radius R circle)
     for i in range(0, 480):
         for j in range(0, 640):
@@ -50,23 +50,18 @@ def vpg_lane_f1(map1, map2, mask_R = 12):
     single_class_f1 = f1_score(extend_mask.flatten(), map1_mask.flatten(), zero_division = 1)
     return single_class_f1
 
-def calcSingleImage(image_path, gt_path, sensivity = 10, mask_R = 20, 
-                    MODEL_FILE = './deploy.prototxt', 
-                    PRETRAINED = 'snapshots/split_iter_100000.caffemodel'):
+def calcSingleImage(mat_path, sensivity = 10, mask_R = 20, CAFFEMODEL = None):
     #==== Load Ground Truth ====
-    mat = loadmat(gt_path) # load mat file
+    mat = loadmat(mat_path) # load mat file
     rgb_seg_vp_label = mat['rgb_seg_vp']
     gt = rgb_seg_vp_label[:, :, 3].reshape(480,640)
-
+    test_img = rgb_seg_vp_label[:, :, :3]/255
     #==== Load the Prediciton ====
     # Set the right path to your model definition file, pretrained model weights,
     # and the image you would like to classify.
     # load the model
-    caffe.set_mode_gpu()
-    caffe.set_device(0)
-    net = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
-    print ("successfully loaded classifier")
-    test_img = caffe.io.load_image(image_path)
+    net = CAFFEMODEL
+#     test_img = caffe.io.load_image(image_path) # Being replaced by load mat directely
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost dimension
     transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
@@ -94,26 +89,31 @@ def calcSingleImage(image_path, gt_path, sensivity = 10, mask_R = 20,
     return score
 
 
-# In[2]:
+# In[49]:
 
 
-gt_path = '/mnt/diska/VPGNet/VPGNet-DB-5ch/scene_3/20160503_0945_43/000091.mat'
-image_path = '/mnt/diska/VPGNet/VPGNet-DB-5ch/scene_3/20160503_0945_43/000091.png'
+#======== Load the necessary parameters ====================================
+### Load the model
 MODEL_FILE = './deploy.prototxt'
 PRETRAINED = 'snapshots/split_iter_100000.caffemodel'
-score = calcSingleImage(image_path, gt_path, sensivity = 50, mask_R = 13, MODEL_FILE = MODEL_FILE, PRETRAINED = PRETRAINED)
+caffe.set_mode_gpu()
+caffe.set_device(0)
+net = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
+print ("successfully loaded classifier")
 
 
-# In[3]:
+# In[50]:
 
 
-for i in score:
-    print(i)
-print(np.average(score))
+#========  Predict on the single image and calculate the f1 score ====================================
+mat_path = '/mnt/diska/VPGNet/VPGNet-DB-5ch/scene_3/20160503_0945_43/000091.mat'
+score = calcSingleImage(mat_path, sensivity = 50, mask_R = 13, CAFFEMODEL = net)
 
 
-# In[ ]:
+# In[51]:
 
 
-
-
+#======== Print the f1 score for each class and the average score ====================================
+for ind, i in enumerate(score):
+    print("{}:{}".format(ind, i))
+print("avg: {}".format(np.average(score)))
